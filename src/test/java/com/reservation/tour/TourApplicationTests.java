80ed45d4fc2aed8801c1ecdaf585c04e121d17ad
YUNDAYOUNG88 <kktok98@naver.com>
2022-11-28 19:46:07 +0900

    Initial commit

---
+++ src/test/java/com/reservation/tour/TourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
